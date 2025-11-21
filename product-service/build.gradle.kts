8645998f32b39053ed50cea76af4c91c4439d074
Vladyslav Zakharzhevskyi <vladyslav.zr@gmail.com>
2025-11-21 17:03:46 +0200

    Add retry from resilience4J

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
     mavenCentral()
 }
 
-val springCloudVersion = "4.2.2"
+val openFeignVersion = "4.2.2"
 val resilience4jVersion = "3.3.0"
 val flywayVersion = "11.15.0"
 val hibernateValidatorVersion = "9.0.1.Final"
@@ -31,12 +31,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-logging")
 
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:${springCloudVersion}")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:${openFeignVersion}")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j:${resilience4jVersion}")
 
     implementation("io.micrometer:micrometer-tracing-bridge-otel")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation ("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
     implementation("io.micrometer:micrometer-registry-otlp")
     implementation("io.github.openfeign:feign-micrometer:${feignMicrometerVersion}")
     implementation("io.micrometer:micrometer-observation")
--- product-service/src/main/java/org/homecorporation/feign/WarehouseClient.java
+++ product-service/src/main/java/org/homecorporation/feign/WarehouseClient.java
@@ -1,5 +1,6 @@
 package org.homecorporation.feign;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.homecorporation.feign.fallback.WarehouseClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(value = "warehouse-service",
-        contextId = "warehouse-service",
+import static org.homecorporation.feign.WarehouseClient.WH_SERVICE_NAME;
+
+@FeignClient(value = WH_SERVICE_NAME, contextId = WH_SERVICE_NAME,
         fallbackFactory = WarehouseClientFallbackFactory.class)
+@Retry(name = WH_SERVICE_NAME)
 public interface WarehouseClient {
+
+    String WH_SERVICE_NAME = "warehouse-service";
 
     @GetMapping("/{ref}")
     Integer getAvailability(@PathVariable(name = "ref") String ref);
