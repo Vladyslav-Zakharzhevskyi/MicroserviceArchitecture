f1612080b8dfdf84e90b88410493782036169953
Vladyslav Zakharzhevskyi <vladyslav.zr@gmail.com>
2025-11-24 14:38:02 +0200

    24.11.2025 Add jdbc observability (mongo) [second]

---
+++ warehouse-service/src/main/java/org/homecorporation/config/ObservabilityConfig.java
@@ -0,0 +1,21 @@
+package org.homecorporation.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.observability.ContextProviderFactory;
+import org.springframework.data.mongodb.observability.MongoObservationCommandListener;
+
+@Configuration
+public class ObservabilityConfig {
+
+    @Bean
+    public MongoClientSettingsBuilderCustomizer mongoMetricsSynchronousContextProvider(ObservationRegistry registry) {
+        return (clientSettingsBuilder) -> {
+            clientSettingsBuilder.contextProvider(ContextProviderFactory.create(registry))
+                    .addCommandListener(new MongoObservationCommandListener(registry));
+        };
+    }
+
+}
